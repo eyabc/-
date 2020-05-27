ca49cc30747076375c788d5aac61f59a63350fe0
eyabc <bey4314@naver.com>
2020-05-27 16:20:00 +0900

    스프링을 통한 의존성 주입 - @resource를 통한 속성 주입

--- src/main/java/expert005/Driver.java
+++ src/main/java/expert005/Driver.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Driver {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("main/java/expert004/expert004.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("main/java/expert005/expert005.xml");
 
         Car car = context.getBean("car", Car.class);
         System.out.println(car.getTireBrand());
