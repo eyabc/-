4ab2907468c9cfcf5cd4809aa961f33f0ba9c8c4
eyabc <bey4314@naver.com>
2020-05-27 15:49:38 +0900

    스프링을 통한 의존성 주입 - @Autowired를 통한 속성 주입

---
+++ src/main/java/expert004/Car.java
@@ -0,0 +1,12 @@
+package main.java.expert004;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Car {
+    @Autowired
+    Tire tire;
+
+    public String getTireBrand() {
+        return "장착된 타이어: " + tire.getBrand();
+    }
+}
---
+++ src/main/java/expert004/Tire.java
@@ -0,0 +1,5 @@
+package main.java.expert004;
+
+public interface Tire {
+    String getBrand();
+}
