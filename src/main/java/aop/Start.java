5945b737167784a3a793e79549431ae6a4c9ded0
eyabc <bey4314@naver.com>
2020-05-28 17:39:04 +0900

    일단 덤벼 보자 - 설명편

--- src/main/java/aop/Start.java
+++ src/main/java/aop/Start.java
@@ -2,13 +2,15 @@ package aop;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 
 public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("file:src/main/java/aop/aop.xml");
 
         Person romeo = context.getBean("boy", Person.class);
+        Person juliet = context.getBean("girl", Person.class);
+
         romeo.runSomething();
+        juliet.runSomething();
     }
 }
