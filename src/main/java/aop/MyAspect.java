456440eb5a54e6f3b269a7da4e2bcf7798b657c9
eyabc <bey4314@naver.com>
2020-05-28 20:18:26 +0900

    After 어드바이스: POJO와 XML 기반 AOP

--- src/main/java/aop/MyAspect.java
+++ src/main/java/aop/MyAspect.java
@@ -7,4 +7,7 @@ public class MyAspect {
         System.out.println("얼굴 인식 확인: 문을 개방하라. ");
 //        System.out.println("열쇠로 문을 열고 집에 들어간다. ");
     }
+    public void lockDoor(JoinPoint joinPoint) {
+        System.out.println("주인님 나갔다! 어이 문 잠가!! ");
+    }
 }
