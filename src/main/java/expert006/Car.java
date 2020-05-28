e5644aa1b3672f757a1c04a2149c0e9694f29951
eyabc <bey4314@naver.com>
2020-05-28 14:12:08 +0900

    사례 연구 4.XML 설정 - 두 개의 빈이 tire 인터페이스를 구현하고 일치하는 id 가 없는 경우 : @Autowired, @Resource 모두 에러

--- src/main/java/expert006/Car.java
+++ src/main/java/expert006/Car.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.Resource;
 
 public class Car {
-    @Resource
+    @Autowired
     Tire tire;
 
     public String getTireBrand() {
