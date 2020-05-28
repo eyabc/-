6f4294702ebdaea07d254cd8fb278af6b52a291c
eyabc <bey4314@naver.com>
2020-05-28 20:12:56 +0900

    일단 덤벼 보자 - POJO와 XML 기반 AOP

--- src/main/java/aop/MyAspect.java
+++ src/main/java/aop/MyAspect.java
@@ -1,14 +1,10 @@
 package aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.JoinPoint;
 
-@Aspect
 public class MyAspect {
-    @Before("execution(* runSomething())")
     public void before(JoinPoint joinPoint) {
         System.out.println("얼굴 인식 확인: 문을 개방하라. ");
-        System.out.println("열쇠로 문을 열고 집에 들어간다. ");
+//        System.out.println("열쇠로 문을 열고 집에 들어간다. ");
     }
 }
